2569ab25818e1ede8d555e4ef0b2c449239877a1
Brayden <160344221+Brayden-H-Wong@users.noreply.github.com>
2024-03-15 11:05:22 -0400

    Update and rename blank.php to index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
 	
 	<!-- link to grid-styles sheet -->
-	<link rel="stylesheet" href="section4/grid_styles.css">
+	<link rel="stylesheet" href="grid_styles.css">
 
 	<!-- links to Google Fonts -->
 	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;1,100;1,300;1,400&display=swap" rel="stylesheet">
@@ -27,7 +27,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Bitter:ital,wght@1,500&display=swap" rel="stylesheet">
 
 	<!-- links to Favicon-->
-	<link rel="shortcut icon" href="../images/Defaultfavicon.png"/>
+	<link rel="shortcut icon" href="images/Defaultfavicon.png"/>
 
 	<!-- link to style sheet -->
 	<link rel="stylesheet" href="blank.css">
